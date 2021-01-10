dae93031591e61109bb8ba998e77f0ada0883bf5
lorenzo-boldrini <56259768+lorenzo-boldrini@users.noreply.github.com>
2021-01-10 20:04:45 +0100

    loading

--- arduino/Assets/scripts/player/character_controller.cs
+++ arduino/Assets/scripts/player/character_controller.cs
@@ -18,6 +18,7 @@ public class character_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.A) && PlayerXpose > -2.8f)
         {
             PlayerXpose -= 1.8f; 
@@ -27,10 +28,39 @@ public class character_controller : MonoBehaviour
             PlayerXpose += 1.8f;
         }
         transform.position = new Vector3(Mathf.Lerp(transform.position.x, PlayerXpose, speed * Time.deltaTime),transform.position.y,transform.position.z);
+        
+        PlayerXpose = -3.6f + (1.8f * numCorsia);
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, PlayerXpose, speed * Time.deltaTime), transform.position.y, transform.position.z);
+        PosController();
     }
 
     public void startToRunAnim()
     {
         _animCon.SetBool("StartGame", true);
+    }
+
+    int numCorsia;
+    void PosController()
+    {
+        float value = input_manager.Singleton.SliderValue;
+           if(value > 0.8f)
+                numCorsia = 4;
+           else if (value > 0.6f && value < 0.8f)
+                numCorsia = 3;
+           
+            else if (value > 0.4f && value < 0.6f)
+                numCorsia = 2;
+        
+            else if(value > 0.2f && value < 0.4f)
+                numCorsia = 1;
+           else
+                numCorsia = 0;
+
+        Debug.Log(numCorsia);
+    }
+
+    public void jump()
+    {
+        Debug.Log("salta puttanella");
     }
 }
